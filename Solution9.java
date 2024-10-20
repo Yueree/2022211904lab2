be8532f8b75dd0e7255b80418d45eeac74a6f112
jy1012 <14233523+jy1012@user.noreply.gitee.com>
2024-10-20 13:40:50 +0800

    Fix bugs in Solution9 and add unit tests

--- Solution9.java
+++ Solution9.java
@@ -36,22 +36,32 @@ import java.util.List;
  * dislikes 中每一组都 不同
  *
  */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution9 {
 
     public boolean possibleBipartition(int n, int[][] dislikes) {
         int[] fa = new int[n + 1];
         Arrays.fill(fa, -1);
         List<Integer>[] g = new List[n + 1];
-        for (int i = 0; i < n; ++i) {
-            g[i] = new ArrayList<Integer>();
+
+        for (int i = 0; i <= n; ++i) {
+            g[i] = new ArrayList<>();
         }
-        for (int[] p : dislikes)
+
+        // 构建图
+        for (int[] p : dislikes) {
             g[p[0]].add(p[1]);
             g[p[1]].add(p[0]);
+        }
+
         for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < g[i].size(); ++j) {
-                unit(g[i].get(0), g[i].get(j), fa);
-                if (isconnect(i, g[i].get(j), fa)) {
+            if (fa[i] == -1) {
+                if (!dfs(i, 0, fa, g)) {
                     return false;
                 }
             }
@@ -59,28 +69,18 @@ class Solution9 {
         return true;
     }
 
-    public void unit(int x, int y, int[] fa) {
-        x = findFa(x, fa);
-        y = findFa(y, fa);
-        if (x == y) {
-            return ;
-        }
-        if (fa[x] <= fa[y]) {
-            int temp = x;
-            x = y;
-            y = temp;
-        }
-        fa[x] += fa[y];
-        fa[y] = x;
-    }
+    private boolean dfs(int node, int color, int[] fa, List<Integer>[] g) {
+        fa[node] = color;
 
-    public boolean isconnect(int x, int y, int[] fa) {
-        x = findFa(x, fa);
-        y = findFa(y, fa);
-        return x == y;
-    }
-
-    public int findFa(int x, int[] fa) {
-        return fa[x] > 0 ? x : (fa[x] = findFa(fa[x], fa));
+        for (int neighbor : g[node]) {
+            if (fa[neighbor] == -1) {
+                if (!dfs(neighbor, 1 - color, fa, g)) {
+                    return false;
+                }
+            } else if (fa[neighbor] == color) {
+                return false;
+            }
+        }
+        return true;
     }
 }
